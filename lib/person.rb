9f140a6ad26c510d09b9ed98f041cdddf2b53305
Carlos Lugo <calugo71@gmail.com>
2022-08-30 17:40:24 -0400

    commit tree 5999ce85a6a8e011dc7dcaf5eb4f95149d70cb28

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,23 @@
 # your code goes here
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+    def initialize(name)
+        @name=name
+        @bank_account=25
+        @happiness=8
+        @hygiene=8
+    end
+    def happiness=(num)
+        @happiness=num.clamp(0, 10)
+    end
+    def hygiene=(num)
+        @hygiene=num.clamp(0, 10)
+    end
+    def happy?
+        self.happiness > 7
+    end
+    def clean?
+        self.hygiene > 7
+    end
+end
